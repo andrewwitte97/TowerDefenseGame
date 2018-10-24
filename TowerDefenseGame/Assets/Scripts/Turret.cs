8cc10e8d59483b300286d4ffaff6b0675f8a489d
andrewwitte97 <awitte97@gmail.com>
2018-10-24 00:17:31 -0400

    afa

--- TowerDefenseGame/Assets/Scripts/Turret.cs
+++ TowerDefenseGame/Assets/Scripts/Turret.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Turret : MonoBehaviour {
 
 	private Transform target;
-	public float range = 15f;
+	[Header("Attributes")]
 	
+	public float range = 15f;
+	public float fireRate = 1f;
+	public float fireCountdown = 0f;
+	
+	
+	[Header("Unity Setup Fields")]
 	public string enemyTag = "Enemy";
 	
 	public Transform partToRotate;
 	public float turnSpeed = 10f;
+	
+
+	public GameObject bulletPrefab;
+	public Transform firePoint;
+	
 	
 	void Start (){
 		InvokeRepeating("UpdateTarget", 0f, .5f);
@@ -56,6 +67,24 @@ public class Turret : MonoBehaviour {
 		partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 		
 		
+		if (fireCountdown <= 0f)
+		{
+			Shoot();
+			fireCountdown = 1f / fireRate;
+		}
+		
+		fireCountdown -= Time.deltaTime;
+		
+		
+	}
+	
+	void Shoot()
+	{
+		GameObject bulletGO = (GameObject) Instantiate (bulletPrefab, firePoint.position, firePoint.rotation);
+		Bullet bullet = bulletGO.GetComponent<Bullet>();
+		
+		if (bullet != null)
+			bullet.Seek(target);
 	}
 	
 	
