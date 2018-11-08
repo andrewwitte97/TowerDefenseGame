d298a2d8770eeafe226b5144b0b7c07b007a3102
andrewwitte97 <awitte97@gmail.com>
2018-11-07 23:10:49 -0500

    fadfadf
    Assets

---
+++ TowerDefenseGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private bool doMovement = true;
+
+	public float panSpeed = 30f;
+	public float panBoarderThickness = 10f;
+	public float scrollSpeed = 5f;
+	public float minY = 10f;
+	public float maxY = 80f;
+	
+	// Update is called once per frame
+	void Update () {
+		//Allow you to hit escape to go off the screen view without moving the camera.
+		if(Input.GetKeyDown (KeyCode.Escape))
+			doMovement = !doMovement;
+		
+		if (!doMovement)
+			return;
+		
+		
+		
+		
+		if (Input.GetKey("d") || Input.mousePosition.y >= Screen.height - panBoarderThickness )
+		{
+			transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+		}
+		
+		if (Input.GetKey("a") || Input.mousePosition.y <= panBoarderThickness )
+		{
+			transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+		}
+		
+		if (Input.GetKey("s") || Input.mousePosition.x >= Screen.width - panBoarderThickness )
+		{
+			transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+		}
+		
+		if (Input.GetKey("w") || Input.mousePosition.x <= panBoarderThickness )
+		{
+			transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+		}
+		//Zoom in and out
+		float scroll = Input.GetAxis("Mouse ScrollWheel");
+		
+		Vector3 pos = transform.position;
+		
+		pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+		pos.y = Mathf.Clamp (pos.y, minY, maxY);
+		
+		
+		transform.position = pos;
+		
+	}
+}
