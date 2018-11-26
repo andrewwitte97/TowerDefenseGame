c7d606f6a4bcb625557d812890088711f3fdf23a
Brandocks <37669059+Brandocks@users.noreply.github.com>
2018-11-26 14:12:32 -0500

    Bugfix 1
    Fixed camera pan issues

--- TowerDefenseGame/Assets/Scripts/CameraController.cs
+++ TowerDefenseGame/Assets/Scripts/CameraController.cs
@@ -6,8 +6,8 @@ public class CameraController : MonoBehaviour {
 
 	private bool doMovement = true;
 
-	public float panSpeed = 30f;
-	public float panBoarderThickness = 10f;
+	public float panSpeed = 5f;
+	public float panBorderThickness = 1f;
 	public float scrollSpeed = 5f;
 	public float minY = 10f;
 	public float maxY = 80f;
@@ -21,36 +21,35 @@ public class CameraController : MonoBehaviour {
 		if (!doMovement)
 			return;
 		
-		
-		
-		
-		if (Input.GetKey("d") || Input.mousePosition.y >= Screen.height - panBoarderThickness )
+		if (Input.GetKey("d") || Input.mousePosition.x >= (Screen.width - panBorderThickness))
 		{
 			transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
 		}
 		
-		if (Input.GetKey("a") || Input.mousePosition.y <= panBoarderThickness )
+		if (Input.GetKey("a") || Input.mousePosition.x <= (panBorderThickness))
 		{
 			transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
 		}
-		
-		if (Input.GetKey("s") || Input.mousePosition.x >= Screen.width - panBoarderThickness )
-		{
-			transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
-		}
-		
-		if (Input.GetKey("w") || Input.mousePosition.x <= panBoarderThickness )
-		{
-			transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
-		}
-		//Zoom in and out
-		float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        //Debug.Log("Screen Width : " + Screen.width);
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= (Screen.height - panBorderThickness))
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= (panBorderThickness))
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        //Zoom in and out
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
 		
 		Vector3 pos = transform.position;
 		
 		pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
 		pos.y = Mathf.Clamp (pos.y, minY, maxY);
-		
 		
 		transform.position = pos;
 		
