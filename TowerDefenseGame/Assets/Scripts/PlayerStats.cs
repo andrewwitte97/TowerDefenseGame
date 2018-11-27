d19480481f394faf5bfe482e2b5e26f7e5dbbd5b
Brandocks <37669059+Brandocks@users.noreply.github.com>
2018-11-27 16:54:12 -0500

    Bugfix 2
    Fixed the bug where the money would not write on the screen properly

--- TowerDefenseGame/Assets/Scripts/PlayerStats.cs
+++ TowerDefenseGame/Assets/Scripts/PlayerStats.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour {
 
 	public static int Money;
 	public int startMoney = 400;
-	
-	void Start()
+    public Text moneyText;
+
+    void Start()
 	{
 		Money = startMoney;
+        moneyText.text = "Money: $" + Money.ToString();
 	}
+
+    void Update()
+    {
+        moneyText.text = "Money: $" + Money.ToString();
+    }
 }
