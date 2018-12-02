0c85eda2ece82f6573031aab0698d09d4186b8dc
Brandocks <37669059+Brandocks@users.noreply.github.com>
2018-12-02 18:25:16 -0500

    Added currency
    back to where we started, except now it works

--- TowerDefenseGame/Assets/Scripts/BuildManager.cs
+++ TowerDefenseGame/Assets/Scripts/BuildManager.cs
@@ -27,56 +27,55 @@ public class BuildManager : MonoBehaviour {
 	public GameObject standardTurretPrefab;
 	public GameObject missleLauncherPrefab;
 	public GameObject buildEffect;
-	private GameObject turretToBuild;
+	private TurretBlueprint turretToBuild;
 
     //Can the turret be built?
-    //public bool CanBuild { get { return turretToBuild != null; } }
+    public bool CanBuild { get { return turretToBuild != null; } }
     //Does the player have enough money to build it?
     //public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
 
     //Called when the game starts.
     //Sets the turret to build on the nodes to the Standard turret.
     //TO-DO: Obsolete method by adding another turret
-    void Start()
+ 
+	
+	public void BuildTurretOn (Node node)
+	{
+        /*
+        //Does the player have enough money to build a turret?
+        if (PlayerStats.Money < turretToBuild.cost)
+        {
+            Debug.Log("Not enough money to build.");
+            return;
+        }
+        
+
+        //Subtract money from the player's balance
+        PlayerStats.Money -= turretToBuild.cost;
+        
+        Debug.Log("Turret built. Money left: " + PlayerStats.Money);
+		*/
+		//Build a turret
+		GameObject turret = (GameObject) Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity); 
+        node.turret = turret;
+			
+        //Add the building effect
+		GameObject effect = (GameObject) Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity); 
+        Destroy(effect, 5f);
+			
+		
+	}
+	
+    //Select which turret to build
+    public void selectTurretToBuild (TurretBlueprint turret)
     {
-        turretToBuild = standardTurretPrefab;    
+        turretToBuild = turret;
     }
 
-    //Returns the turret to build on the node
-    public GameObject getTurretToBuild()
+    public TurretBlueprint GetTurretToBuild()
     {
         return turretToBuild;
     }
-	
-	//public void BuildTurretOn (Node node)
-	//{
-        //Does the player have enough money to build a turret?
-		//if (PlayerStats.Money < turretToBuild.cost)
-		//{
-		//	Debug.Log("Not enought money to build!");
-		//	return;
-		//}
-		
-        //Subtract money from the player's balance
-		//PlayerStats.Money -= turretToBuild.cost;
-        
-		
-		//Build a turret
-		//GameObject turret = Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity); 
-       // node.turret = turret;
-			
-        //Add the building effect
-		//GameObject effect = Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity); 
-       // Destroy(effect, 5f);
-			
-		//Debug.Log ("Turret build! Money Left: " + PlayerStats.Money);
-	//}
-	
-    //Select which turret to build
-	//public void SelectTurretToBuild (TurretBlueprint turret)
-	//{
-		//turretToBuild = turret;
-	//}
 
 
 }
--- TowerDefenseGame/Assets/Scripts/TurretBlueprint.cs
+++ TowerDefenseGame/Assets/Scripts/TurretBlueprint.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+//Unity will save and load values in this class for us,
+//which makes them visible in inspector.
 [System.Serializable]
+
+//Saves values for use with selecting the turrets in the shop
 public class TurretBlueprint {
 
 	public GameObject prefab;
